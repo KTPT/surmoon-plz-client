e0d51e4fa09981f97e73cac200a48d615c2e6df6
hwookim <gusdn9754@gmail.com>
2021-03-22 01:25:24 +0900

    feat: linebreak 옵션 추가
     - false 시 enter 키 입력 불가
     - 붙여넣기 시 개행 제거

--- src/components/SurveyTextArea.tsx
+++ src/components/SurveyTextArea.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { TextareaAutosize } from '@material-ui/core';
 
 interface Props {
   placeholder?: string;
   defaultValue?: string;
+  linebreak?: boolean;
 }
 
 const Container = styled(TextareaAutosize)`
@@ -23,6 +24,34 @@ const Container = styled(TextareaAutosize)`
 export default function SurveyTextArea({
   placeholder = '',
   defaultValue = '',
+  linebreak = true,
 }: Props): JSX.Element {
-  return <Container placeholder={placeholder} defaultValue={defaultValue} />;
+  const [value, setValue] = useState<string>(defaultValue);
+
+  const onKeyPress = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ): void => {
+    if (event.key !== 'Enter' || linebreak) {
+      return;
+    }
+    event.preventDefault();
+  };
+
+  const onChange = ({
+    target,
+  }: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    const newValue = linebreak
+      ? target.value
+      : target.value.replace(/(\r\n|\n|\r)/gm, '');
+    setValue(newValue);
+  };
+
+  return (
+    <Container
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      onKeyPress={onKeyPress}
+    />
+  );
 }
