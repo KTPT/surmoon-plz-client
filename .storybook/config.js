83ee4891655dffcaea67c7499fb4c2fd28e1332c
Dahee Kim <40848918+warmwhiten@users.noreply.github.com>
2021-03-21 22:02:13 +0900

    카드 컴포넌트 MVP 구현 (#7)
    * feat: storybook 설정 변경
    
    * feat: 카드 컴포넌트 기본 형태 추가

--- .storybook/config.js
+++
@@ -1,3 +0,0 @@
-import { configure } from '@storybook/react';
-// automatically import all files ending in *.storybook.tsx
-configure(require.context('../storybook', true, /\.stories\.tsx?$/), module);
