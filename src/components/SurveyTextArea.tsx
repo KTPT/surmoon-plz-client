0ded2ab122bc9fac2007fe784f4a7c9cb39dbf58
hwookim <gusdn9754@gmail.com>
2021-03-21 23:18:07 +0900

    style: 스타일 적용
     - resize 불가
     - 하단을 제외한 외곽선 삭제
     - 포커스 시 트랜지션 적용

--- src/components/SurveyTextArea.tsx
+++ src/components/SurveyTextArea.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 import { TextareaAutosize } from '@material-ui/core';
 
+const Container = styled(TextareaAutosize)`
+  width: 100%;
+  border: none;
+  border-bottom: solid 2px #e0e0e0;
+  outline: none;
+  resize: none;
+  transition: border-bottom ease-out 0.5s;
+
+  &:focus {
+    border-bottom: 2px solid #673ab7;
+  }
+`;
+
 export default function SurveyTextArea(): JSX.Element {
-  return <TextareaAutosize />;
+  return <Container />;
 }
